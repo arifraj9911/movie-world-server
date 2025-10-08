702e55a297e43dc5704721010d7eb0fea1369a07
arif-dev199 <arif@pixfar.com>
2025-10-08 13:44:25 +0600

    live link added

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 // parser
 app.use(express.json());
 // app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000","https://movie-world-client.vercel.app"], credentials: true }));
 
 // application route
 app.use("/api/v1", router);
